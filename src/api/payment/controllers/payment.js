a593540073f35463882c80a6a587f9d0df8dd0a4
Pedro Goiânia <pedro.paulods@hotmail.com>
2024-08-05 19:12:08 -0300

    feat(config): created the entities for payment and event

---
+++ src/api/payment/controllers/payment.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/**
+ * payment controller
+ */
+
+// d012e1f543eb327286ad1f5001cac58d9b26347a4f5c714217630b4013940a1529d6ad55ec7f22147e9f7df6a1957fb3cebdb8902a2ad0561b77e0dcfde2fd20b75f23818f7283c42f1fada843213722e6297f1dd877a064fe3bc3a427590f3877682af00d99747f783a68dc0b4c64427d0a86486d0805d972090e94aa300b7d
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::payment.payment');
