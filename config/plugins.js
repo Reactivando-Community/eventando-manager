d8f91f9d1c63b4e0a5d05667e83838ffb8ccec24
Pedro Goiânia <pedro.paulods@hotmail.com>
2025-08-27 19:43:28 -0300

    fixed email provider

--- config/plugins.js
+++ config/plugins.js
@@ -9,7 +9,7 @@ module.exports = () => ({
           user: "eventando@8020digital.com.br",
           pass: "F1r3wall@0212",
         },
-        secure: true
+        secure: false,
       },
       settings: {
         defaultFrom: "contato@8020digital.com.br",
